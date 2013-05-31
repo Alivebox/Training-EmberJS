03b3a685562a90d888729250af9e473f0fa20187
juank11memphis <juank.memphis@gmail.com>
2013-05-31 15:33:34 -0600

    Master-template view with routing

---
+++ example1/entrypoint.js
@@ -0,0 +1,36 @@
+require.config(
+    {
+
+        paths:{
+            emberjs:['//cdnjs.cloudflare.com/ajax/libs/ember.js/1.0.0-rc.4/ember.min','resources/js/ember'],
+            jquery: ['//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min','resources/js/jquery'],
+            handlebars: ['//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0-rc.4/handlebars.min','resources/js/handlebars'],
+            application: 'app/app'
+        },
+
+        shim: {
+            'emberjs': {
+                deps: [
+                    'jquery',
+                    'handlebars'
+                ],
+                exports: 'Ember'
+            },
+            'application': {
+                deps: [
+                    'emberjs'
+                ]
+            }
+        }
+
+    }
+);
+
+require(
+    [
+        'application'
+    ],
+    function(){
+        console.log('EntryPoint ready!!!');
+    }
+);
